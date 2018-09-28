2d2fcd20e1e9d5f527972c78107d49f734fea3e8
dina aid <dinaaid3@gmail.com>
2018-09-28 18:47:53 +0200

    version.01

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var slideshow = document.getElementById('slideshow');
+var next = document.getElementById('next');
+var pre = document.getElementById('pre');
+
+function preImage() {
+    slideshow.style.backgroundImage = "url('../assets/vw1.jpg')";
+}
+
+function nextImage() {
+    slideshow.style.backgroundImage = "url('../assets/vw3.jpg')";
+}
